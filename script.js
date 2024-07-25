85a14ae5d88b0c6fde1411d928831dbfefc216e8
gorz <c.rodrigoalvz@gmail.com>
2024-07-25 05:24:17 -0300

    add css

--- script.js
+++ script.js
@@ -1,62 +1,62 @@
-const numbers = document.querySelectorAll('.number')
-const operators = document.querySelectorAll('.operator')
-const equals = document.querySelector('.equals')
-const clear = document.querySelector('.clear')
-const display = document.getElementById('display')
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
+const display = document.getElementById("display");
 
-let displayX = ''
-let displayY = ''
-let displayOp = ''
-let nextOperator = ''
+let displayX = "";
+let displayY = "";
+let displayOp = "";
+let nextOperator = "";
 
-numbers.forEach(number => {
-  number.addEventListener('click', (e) => {
-    if (displayOp === '') {
-      displayX += e.target.textContent 
-      displayFill()
-    }else{
-      displayY += e.target.textContent 
-      displayFill()
+numbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    if (displayOp === "") {
+      displayX += e.target.textContent;
+      displayFill();
+    } else {
+      displayY += e.target.textContent;
+      displayFill();
     }
   });
 });
 
-operators.forEach(operator => {
-  operator.addEventListener('click', (e) => {
-    if (displayX !== '' && displayY === '') {
-      displayOp = e.target.textContent
-      displayFill()
-    } else if (displayY !== '') {
-      nextOperator = e.target.textContent
-      displayFill()
+operators.forEach((operator) => {
+  operator.addEventListener("click", (e) => {
+    if (displayX !== "" && displayY === "") {
+      displayOp = e.target.textContent;
+      displayFill();
+    } else if (displayY !== "") {
+      nextOperator = e.target.textContent;
+      displayFill();
     }
   });
 });
 
-clear.addEventListener('click', () => clearData())
+clear.addEventListener("click", () => clearData());
 
-equals.addEventListener('click', () => {
-  if (displayX !== '' && displayY !== '' && displayOp !== '') {
-    let res = operate(Number(displayX), Number(displayY), displayOp)
-    let next = nextOperator
-    clearData()
-    displayX = res
-    displayOp = next
-    displayFill()
+equals.addEventListener("click", () => {
+  if (displayX !== "" && displayY !== "" && displayOp !== "") {
+    let res = operate(Number(displayX), Number(displayY), displayOp);
+    let next = nextOperator;
+    clearData();
+    displayX = res;
+    displayOp = next;
+    displayFill();
   }
-})
+});
 
 const displayFill = () => {
-  display.textContent = `${displayX} ${displayOp} ${displayY} ${nextOperator}`
-}
+  display.textContent = `${displayX} ${displayOp} ${displayY} ${nextOperator}`;
+};
 
 const clearData = () => {
-  displayX = ''
-  displayY = ''
-  displayOp = ''
-  nextOperator = ''
-  displayFill()
-}
+  displayX = "";
+  displayY = "";
+  displayOp = "";
+  nextOperator = "";
+  displayFill();
+};
 
 const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
@@ -65,15 +65,15 @@ const divide = (x, y) => (y === 0 ? "ERROR" : x / y);
 
 const operate = (x, y, op) => {
   switch (op) {
-    case '+':
-      return add(x, y)
-    case '-':
-      return subtract(x, y)
-    case '*':
-      return multiply(x, y)
-    case '/':
-      return divide(x, y)
+    case "+":
+      return add(x, y);
+    case "-":
+      return subtract(x, y);
+    case "*":
+      return multiply(x, y);
+    case "/":
+      return divide(x, y);
     default:
       break;
   }
-}
+};
